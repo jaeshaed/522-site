d0fd52bf414d236ee5ff6485ee22a5a5017ae5ad
Roman Alexandrov <wefewf325@mail.ru>
2025-03-12 13:51:50 +0300

    добавил

---
+++ appe.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+appe = Flask(__name__)
+@appe.route('/')
+def foot():
+    return render_template('index.html')
+
+if __name__ =='__main__':
+    appe.run(debug=True)
